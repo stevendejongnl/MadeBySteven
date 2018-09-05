545629da96531b095ba4270c6db84508a29747a2
Steven de Jong <steven@steven-dejong.nl>
2018-09-05 15:04:32 +0200

    PHP routing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,18 @@
 // eslint-disable-next-line
-import Home from './components/home.js';
-// eslint-disable-next-line
 import Navigation from './components/navigation.js';
+// eslint-disable-next-line
+import Home from './components/home.js';
 
-const HOME = new Home();
+const PAGE = {
+  home: (!!document.querySelector('.page-home')),
+  about: (!!document.querySelector('.page-about')),
+  project: (!!document.querySelector('.page-project')),
+};
+
 const NAVIGATION = new Navigation();
-
-HOME.init();
 NAVIGATION.init();
+
+if (PAGE.home) {
+  const HOME = new Home();
+  HOME.init();
+}
---
+++ views/pages/project.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Made by Steven | Project</title>
+
+    <link rel="stylesheet" href="../../dist/styles/main.css">
+</head>
+<body class="page-project">
+
+<script src="../../src/scripts/main.js" type="module"></script>
+</body>
+</html>
