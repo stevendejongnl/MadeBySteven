2a5a11208f025041f2f3fcb5c148d569e91425ec
Steven de Jong <steven@steven-dejong.nl>
2018-09-06 08:53:59 +0200

    split index into view files

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $pageInfo = new PageInfo();
 $pageInfo->page = '404';
 $pageInfo->title = 'Page not found';
 $pageInfo->classes = 'page-404';
+
 if ($path === '/') {
     $pageInfo->page = 'home';
 	$pageInfo->title = 'Made by Steven';
@@ -25,25 +26,7 @@ if ($path === '/') {
 	$pageInfo->title = 'Made by Steven | Project';
 	$pageInfo->classes = 'page-project';
 }
-?>
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $pageInfo->title; ?></title>
 
-    <link rel="stylesheet" href="./dist/styles/main.css">
-</head>
-<body class="<?= $pageInfo->classes; ?>">
-
-<main role="main" class="main-container">
-    <? include_once 'views/parts/header.php'; ?>
-    <? include_once 'views/pages/' . $pageInfo->page . '.php'; ?>
-</main>
-
-<script src="./src/scripts/main.js" type="module"></script>
-</body>
-</html>
+include_once 'views/parts/head.php';
+include_once 'views/pages/' . $pageInfo->page . '.php';
+include_once 'views/parts/footer.php';
---
+++ views/parts/head.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title><?= $pageInfo->title; ?></title>
+
+	<link rel="stylesheet" href="./dist/styles/main.css">
+</head>
+<body class="<?= $pageInfo->classes; ?>">
+
+<main role="main" class="main-container">
+	<? include_once 'views/parts/header.php'; ?>
