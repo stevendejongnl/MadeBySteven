7a4efd3a03a8341cce318e82aeb928223aebb980
Steven de Jong <steven@steven-dejong.nl>
2018-09-07 09:02:28 +0200

    About page done

--- views/pages/about.php
+++ views/pages/about.php
@@ -2,59 +2,72 @@
     <h2 class="heading--second">Full-Stack<br class="hide--for-desktop"> Developer</h2>
 
     <div class="wrapper">
-        <article>
-            <h1>Steven</h1>
-            <p>Ik ben een gedreven en positief ingestelde Full-stack Developer. Zo heb ik veel ervaring op het gebied van PHP, MySQL, JavaScript (ES5/6), Zurb Foundation en Bootstrap.</p>
-            <p>Door mijn ervaringen als vormgever waarbij ik o.a. gebruik gemaakt heb van Adobe Photoshop, Illustrator en Sketch kan ik makkelijk schakelen en communiceren in een multidisciplinair team.</p>
+        <article class="about-me">
+            <h1 class="heading--first">Steven</h1>
+            <p class="paragraph">Ik ben een gedreven en positief ingestelde Full-stack Developer. Zo heb ik veel ervaring op het gebied van PHP, MySQL, JavaScript (ES5/6), Zurb Foundation en Bootstrap.</p>
+            <p class="paragraph">Door mijn ervaringen als vormgever waarbij ik o.a. gebruik gemaakt heb van Adobe Photoshop, Illustrator en Sketch kan ik makkelijk schakelen en communiceren in een multidisciplinair team.</p>
         </article>
 
         <div class="love-to-work">
-            <h3>I love to work with</h3>
-            <ul class="list--reset">
-                <li>
-                    <? Functions::getSVG('grunt'); ?>
-                </li>
-                <li>
-                    <? Functions::getSVG('bootstrap'); ?>
-                </li>
-                <li>
+            <h3 class="heading--third">I love to work with</h3>
+
+            <ul class="list--reset love-to-work__list">
+                <li class="love-to-work__item">
                     <? Functions::getSVG('angular'); ?>
                 </li>
-                <li>
-                    <? Functions::getSVG('grunt'); ?>
+                <li class="love-to-work__item">
+                    <? Functions::getSVG('bootstrap'); ?>
                 </li>
-                <li>
+                <li class="love-to-work__item">
                     <? Functions::getSVG('css3'); ?>
                 </li>
-                <li>
+                <li class="love-to-work__item">
+                    <? Functions::getSVG('grunt'); ?>
+                </li>
+                <li class="love-to-work__item">
                     <? Functions::getSVG('gulp'); ?>
                 </li>
-                <li>
+                <li class="love-to-work__item">
                     <? Functions::getSVG('javascript'); ?>
                 </li>
-                <li>
+                <li class="love-to-work__item">
                     <? Functions::getSVG('jquery'); ?>
                 </li>
-                <li>
+                <li class="love-to-work__item">
                     <? Functions::getSVG('laravel'); ?>
                 </li>
-                <li>
+                <li class="love-to-work__item">
                     <? Functions::getSVG('mysql'); ?>
                 </li>
-                <li>
+                <li class="love-to-work__item">
                     <? Functions::getSVG('php'); ?>
                 </li>
-                <li>
+                <li class="love-to-work__item">
                     <? Functions::getSVG('postgresql'); ?>
                 </li>
-                <li>
+                <li class="love-to-work__item">
                     <? Functions::getSVG('sass'); ?>
                 </li>
-                <li>
+                <li class="love-to-work__item">
                     <? Functions::getSVG('shopify'); ?>
                 </li>
-                <li>
+                <li class="love-to-work__item">
                     <? Functions::getSVG('wordpress'); ?>
+                </li>
+            </ul>
+
+            <h4 class="heading--fourth">Get in touch</h4>
+            <ul class="list--reset social">
+                <li class="social__item">
+                    <a href="https://github.com/stevendejongnl" target="_blank" class="social__link">
+		                <? Functions::getSVG('github'); ?>
+                    </a>
+                </li>
+
+                <li class="social__item">
+                    <a href="https://www.linkedin.com/in/stevendejongnl" target="_blank" class="social__link">
+			            <? Functions::getSVG('linkedin'); ?>
+                    </a>
                 </li>
             </ul>
         </div>
