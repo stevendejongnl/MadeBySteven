91be59c2d28507ee4d3580598ae235b3f9b7e0cd
Steven de Jong <steven@steven-dejong.nl>
2018-09-10 10:37:38 +0200

    Project page

--- src/scripts/components/project.js
+++ src/scripts/components/project.js
@@ -15,51 +15,51 @@ export default class Project {
     this.generatePositions();
     this.generateGrid();
 
-    this.gallery.container.addEventListener('click', (event) => {
-      if (event.target.className !== 'image__plus') {
-        event.preventDefault();
-        this.gallery.items.forEach((item) => {
-          item.classList.remove('image--open');
-          item.classList.add('image--closed');
-        });
-      }
-    });
+    // this.gallery.container.addEventListener('click', (event) => {
+    //   if (event.target.className !== 'image__plus') {
+    //     event.preventDefault();
+    //     this.gallery.items.forEach((item) => {
+    //       item.classList.remove('image--open');
+    //       item.classList.add('image--closed');
+    //     });
+    //   }
+    // });
 
     this.gallery.items.forEach((item) => {
-      item.classList.remove('image--open');
-      item.classList.add('image--closed');
+      item.classList.add('image--open');
+      item.classList.remove('image--closed');
 
-      item.childNodes[1].addEventListener('click', (event) => {
-        event.preventDefault();
-
-        if (event.target.className === 'image__plus') {
-          item.classList.toggle('image--closed');
-          item.classList.toggle('image--open');
-        }
-      });
-      item.childNodes[3].addEventListener('click', (event) => {
-        event.preventDefault();
-
-        event.path.forEach((element) => {
-          if (element.className === 'image__container') {
-            window.location = element.getAttribute('href');
-          }
-        });
-      });
-      if (window.innerWidth >= 640) {
-        item.childNodes[1].addEventListener('mouseenter', (event) => {
-          event.preventDefault();
-
-          item.classList.remove('image--closed');
-          item.classList.add('image--open');
-        });
-        item.childNodes[3].addEventListener('mouseleave', (event) => {
-          event.preventDefault();
-
-          item.classList.add('image--closed');
-          item.classList.remove('image--open');
-        });
-      }
+      // item.childNodes[1].addEventListener('click', (event) => {
+      //   event.preventDefault();
+      //
+      //   if (event.target.className === 'image__plus') {
+      //     item.classList.toggle('image--closed');
+      //     item.classList.toggle('image--open');
+      //   }
+      // });
+      // item.childNodes[3].addEventListener('click', (event) => {
+      //   event.preventDefault();
+      //
+      //   event.path.forEach((element) => {
+      //     if (element.className === 'image__container') {
+      //       window.location = element.getAttribute('href');
+      //     }
+      //   });
+      // });
+      // if (window.innerWidth >= 640) {
+      //   item.childNodes[1].addEventListener('mouseenter', (event) => {
+      //     event.preventDefault();
+      //
+      //     item.classList.remove('image--closed');
+      //     item.classList.add('image--open');
+      //   });
+      //   item.childNodes[3].addEventListener('mouseleave', (event) => {
+      //     event.preventDefault();
+      //
+      //     item.classList.add('image--closed');
+      //     item.classList.remove('image--open');
+      //   });
+      // }
     });
 
     window.addEventListener('resize', () => {
@@ -71,13 +71,8 @@ export default class Project {
   generatePositions() {
     this.gallery.items.forEach((item) => {
       let rowPos = item.getAttribute('data-row');
-      rowPos = rowPos.split(',');
 
-      if (window.innerWidth < 640) {
-        item.setAttribute('style', `grid-row:${rowPos[0]};`);
-      } else {
-        item.setAttribute('style', `grid-row:${rowPos[1]}`);
-      }
+      item.setAttribute('style', `grid-row:${rowPos};`);
     });
   }
 
@@ -103,9 +98,14 @@ export default class Project {
       gridWidth = 5;
     }
 
+    let totalRows = 4;
+    if (window.innerWidth < 640) {
+      totalRows = 6;
+    }
+
     const GRIDHEIGHT = Number((
       (this.gallery.container.offsetHeight / this.item.height)
-      + 2
+      * totalRows
     ).toFixed());
 
     this.times(gridWidth * GRIDHEIGHT)(() => {
--- views/parts/gallery.php
+++ views/parts/gallery.php
@@ -1,68 +1,95 @@
 <div class="gallery-holder">
 	<div class="project-gallery" id="galleryContainer">
-		<article class="image" data-row="5,3" data-column="9">
-			<div class="image__plus" data-pos="5">
-				<h3 class="image__title">Project 1</h3>
-			</div>
+		<article class="image" data-row="2" data-column="9">
+			<div class="image__plus" data-pos="5"></div>
 
-			<a href="/project" class="image__container">
+			<div class="image__container">
 				<figure class="image__figure">
 					<div class="image__figure-inner">
 						<img src="https://picsum.photos/1600/800?image=2" class="image__image" alt="Project 1">
-						<span class="image__view">View</span>
 					</div>
-					<figcaption class="image__figcaption">Project 1</figcaption>
+					<figcaption class="image__figcaption">Lorem ipsum dolor sit amet, consectetur. A at autem dignissimos doloremque dolorum, hic illum ipsam ipsum iusto laboriosam magni nihil perspiciatis placeat sapiente sunt veniam voluptate. Itaque, quod.</figcaption>
 				</figure>
-			</a>
+			</div>
 		</article>
 
-		<article class="image" data-row="6,4" data-column="4">
-			<div class="image__plus" data-pos="0">
-				<h3 class="image__title">Project 2</h3>
-			</div>
+		<article class="image" data-row="3" data-column="4">
+			<div class="image__plus" data-pos="0"></div>
 
-			<a href="/project" class="image__container">
+			<div class="image__container">
 				<figure class="image__figure">
 					<div class="image__figure-inner">
 						<img src="https://picsum.photos/1600/800?image=3" class="image__image" alt="Project 2">
-						<span class="image__view">View</span>
 					</div>
-					<figcaption class="image__figcaption">Project 2</figcaption>
+					<figcaption class="image__figcaption">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda facere impedit sint. Asperiores beatae delectus deserunt ea eligendi eos harum laudantium odio optio quis quo soluta suscipit unde veritatis, voluptatibus?</figcaption>
 				</figure>
-			</a>
+			</div>
 		</article>
 
-		<article class="image" data-row="7,5" data-column="7">
-			<div class="image__plus" data-pos="3">
-				<h3 class="image__title">Project 3</h3>
-			</div>
+		<article class="image" data-row="4" data-column="7">
+			<div class="image__plus" data-pos="3"></div>
 
-			<a href="/project" class="image__container">
+			<div class="image__container">
 				<figure class="image__figure">
 					<div class="image__figure-inner">
 						<img src="https://picsum.photos/1600/800?image=4" class="image__image" alt="Project 3">
-						<span class="image__view">View</span>
 					</div>
-					<figcaption class="image__figcaption">Project 3</figcaption>
+					<figcaption class="image__figcaption">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate ducimus eum incidunt ipsam omnis perferendis placeat possimus voluptatum? At consequuntur deserunt, impedit modi molestias nam?</figcaption>
 				</figure>
-			</a>
+			</div>
 		</article>
 
-		<article class="image" data-row="8,6" data-column="2">
-			<div class="image__plus" data-pos="4">
-				<h3 class="image__title">Project 4</h3>
-			</div>
+		<article class="image" data-row="5" data-column="2">
+			<div class="image__plus" data-pos="4"></div>
 
-			<a href="/project" class="image__container">
+			<div class="image__container">
 				<figure class="image__figure">
 					<div class="image__figure-inner">
 						<img src="https://picsum.photos/1600/800?image=5" class="image__image" alt="Project 4">
-						<span class="image__view">View</span>
 					</div>
-					<figcaption class="image__figcaption">Project 4</figcaption>
+					<figcaption class="image__figcaption">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad aliquid eos explicabo, harum inventore odio praesentium quod, ratione sapiente sequi tempora tenetur vel! Error explicabo!</figcaption>
 				</figure>
-			</a>
+			</div>
 		</article>
+
+        <article class="image" data-row="6" data-column="4">
+            <div class="image__plus" data-pos="4"></div>
+
+            <div class="image__container">
+                <figure class="image__figure">
+                    <div class="image__figure-inner">
+                        <img src="https://picsum.photos/1600/800?image=6" class="image__image" alt="Project 5">
+                    </div>
+                    <figcaption class="image__figcaption">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ducimus eius iure nesciunt nostrum placeat rerum veniam? Amet animi blanditiis debitis error est laboriosam.</figcaption>
+                </figure>
+            </div>
+        </article>
+
+        <article class="image" data-row="7" data-column="2">
+            <div class="image__plus" data-pos="1"></div>
+
+            <div class="image__container">
+                <figure class="image__figure">
+                    <div class="image__figure-inner">
+                        <img src="https://picsum.photos/1600/800?image=7" class="image__image" alt="Project 6">
+                    </div>
+                    <figcaption class="image__figcaption">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ducimus eius iure nesciunt nostrum placeat rerum veniam? Amet animi blanditiis debitis error est laboriosam.</figcaption>
+                </figure>
+            </div>
+        </article>
+
+        <article class="image" data-row="8" data-column="5">
+            <div class="image__plus" data-pos="3"></div>
+
+            <div class="image__container">
+                <figure class="image__figure">
+                    <div class="image__figure-inner">
+                        <img src="https://picsum.photos/1600/800?image=8" class="image__image" alt="Project 7">
+                    </div>
+                    <figcaption class="image__figcaption">Lorem ipsum dolor sit amet, consectetur adipisicing elit. At ducimus eius iure nesciunt nostrum placeat rerum veniam? Amet animi blanditiis debitis error est laboriosam.</figcaption>
+                </figure>
+            </div>
+        </article>
 	</div>
 
 	<div class="grid grid--red  " id="fillGrid"></div>
