30086dfad606d5054a4ffd22ef7ad5575783918f
stevendejongnl <stevendejongnl@users.noreply.github.com>
2023-05-27 21:30:35 +0000

    deploy: 1215c5c80f4e56011e4e435f7546232d35a2b724

--- components/made-a-menu/made-a-menu.d.ts
+++ components/made-a-menu/made-a-menu.d.ts
@@ -1,4 +1,5 @@
 import { LitElement } from 'lit';
 export declare class MadeAMenu extends LitElement {
+    static styles: import("lit").CSSResult;
     render(): import("lit-html").TemplateResult<1>;
 }
--- elements/made-a-menu-item/made-a-menu-item.d.ts
+++ elements/made-a-menu-item/made-a-menu-item.d.ts
@@ -1,4 +1,6 @@
 import { LitElement } from 'lit';
 export declare class MadeAMenuItem extends LitElement {
+    static styles: import("lit").CSSResult;
+    url: string;
     render(): import("lit-html").TemplateResult<1>;
 }
