ce18459d84b78219f1b28a6ddff0a55708c4d75d
🍟 <steven@steven-dejong.nl>
2023-05-29 11:30:37 +0200

    Use local storage

--- src/components/made-a-github-overview/made-a-github-overview.ts
+++ src/components/made-a-github-overview/made-a-github-overview.ts
@@ -1,6 +1,5 @@
 import {html, LitElement} from 'lit'
 import {customElement} from 'lit/decorators.js'
-
 import {style} from './made-a-github-overview.style.js'
 import {Repository} from '../made-a-github-repository/made-a-github-repository.interface.js'
 
@@ -12,7 +11,13 @@ export class MadeAGithubOverview extends LitElement {
   static override styles = style
 
   override async firstUpdated() {
-    await this.fetchRepositories()
+    const repositories = this.getRepositoriesFromLocalStorage()
+    if (repositories) {
+      this._repositories = repositories
+      this.requestUpdate()
+    } else {
+      await this.fetchRepositories()
+    }
   }
 
   private _repositories: Repository[] = []
@@ -27,13 +32,43 @@ export class MadeAGithubOverview extends LitElement {
       if (!response.ok) {
         throw new Error('Failed to fetch repositories')
       }
-      this._repositories = await response.json().then((data: Repository[]) => data.sort((a, b) => {
+      const repositories = await response.json().then((data: Repository[]) => data.sort((a, b) => {
         return new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
       }))
+      this._repositories = repositories
+      this.saveRepositoriesToLocalStorage(repositories)
       this.requestUpdate()
     } catch (error) {
       console.error(error)
     }
+  }
+
+  private saveRepositoriesToLocalStorage(repositories: Repository[]): void {
+    const storageKey = 'repositories'
+    const storageValue = JSON.stringify({
+      data: repositories,
+      expiration: Date.now() + 24 * 60 * 60 * 1000 // Vervaldatum na 24 uur (in milliseconden)
+    })
+    localStorage.setItem(storageKey, storageValue)
+  }
+
+  private getRepositoriesFromLocalStorage(): Repository[] | null {
+    const storageKey = 'repositories'
+    const storageValue = localStorage.getItem(storageKey)
+    if (storageValue) {
+      const repositoriesData = JSON.parse(storageValue)
+      if (this.isRepositoriesExpired(repositoriesData)) {
+        localStorage.removeItem(storageKey)
+        return null
+      }
+      return repositoriesData.data
+    }
+    return null
+  }
+
+  private isRepositoriesExpired(repositoriesData: {data: Repository[], expiration: number}): boolean {
+    const expiration = repositoriesData.expiration
+    return Date.now() > expiration
   }
 
   override render() {
--- src/components/made-a-github-repository/made-a-github-repository.ts
+++ src/components/made-a-github-repository/made-a-github-repository.ts
@@ -1,6 +1,5 @@
 import {html, LitElement} from 'lit'
 import {customElement, property} from 'lit/decorators.js'
-
 import {style} from './made-a-github-repository.style.js'
 import {Languages, Repository} from './made-a-github-repository.interface.js'
 
@@ -12,7 +11,13 @@ export class MadeAGithubRepository extends LitElement {
   repository: Repository | null
 
   override async firstUpdated() {
-    await this.fetchLanguages()
+    const languages = this.getLanguagesFromLocalStorage()
+    if (languages) {
+      this._languages = languages
+      this.requestUpdate()
+    } else {
+      await this.fetchLanguages()
+    }
   }
 
   private _languages: Languages = {}
@@ -28,16 +33,46 @@ export class MadeAGithubRepository extends LitElement {
         if (!response.ok) {
           throw new Error('Failed to fetch languages')
         }
-        this._languages = await response.json()
+        const languages = await response.json()
+        this._languages = languages
+        this.saveLanguagesToLocalStorage(languages)
         this.requestUpdate()
       } catch (error) {
         console.error(error)
       }
     }
   }
+
+  private saveLanguagesToLocalStorage(languages: Languages): void {
+    const storageKey = `languages_${this.repository?.name}`
+    const storageValue = JSON.stringify({
+      data: languages,
+      expiration: Date.now() + 24 * 60 * 60 * 1000 // Vervaldatum na 24 uur (in milliseconden)
+    })
+    localStorage.setItem(storageKey, storageValue)
+  }
+
+  private getLanguagesFromLocalStorage(): Languages | null {
+    const storageKey = `languages_${this.repository?.name}`
+    const storageValue = localStorage.getItem(storageKey)
+    if (storageValue) {
+      const languagesData = JSON.parse(storageValue)
+      if (this.isLanguagesExpired(languagesData)) {
+        localStorage.removeItem(storageKey)
+        return null
+      }
+      return languagesData.data
+    }
+    return null
+  }
+
+  private isLanguagesExpired(languagesData: {data: Languages, expiration: number}): boolean {
+    const expiration = languagesData.expiration
+    return Date.now() > expiration
+  }
+
   constructor() {
     super()
-
     this.repository = null
   }
 
@@ -47,18 +82,15 @@ export class MadeAGithubRepository extends LitElement {
         <a class="title" target="_blank" href="${this.repository?.html_url}">
           ${this.repository?.name}
         </a>
-          
         <div class="description">
           <p>${this.repository?.description}</p>
         </div>
-          
         <footer>
           <ul class="languages">
             ${Object.entries(this.languages).map(([language]) => html`
               <li>${language}</li>
             `)}
           </ul>
-          
           <div class="stars">
             ${this.repository?.stargazers_count || null}
           </div>
