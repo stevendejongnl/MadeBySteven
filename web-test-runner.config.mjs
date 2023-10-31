f4b6c01b1015458088325c4fb9281ae7f7d79517
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2023-10-31 09:45:14 +0100

    test: add junit test reporter

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,19 +1,47 @@
-/* jshint esversion: 6 */
+/* jshint node: true */
+/* jshint esversion: 2020 */
 /* jshint strict: false */
 
-import {env} from 'process';
-import {playwrightLauncher} from '@web/test-runner-playwright';
-import {esbuildPlugin} from '@web/dev-server-esbuild';
+import { defaultReporter } from '@web/test-runner'
+import { junitReporter } from '@web/test-runner-junit-reporter'
+import { playwrightLauncher } from '@web/test-runner-playwright'
+
+const browsers = {
+  chromium: playwrightLauncher({ product: 'chromium' }),
+  firefox: playwrightLauncher({ product: 'firefox', concurrency: 1 }),
+  webkit: playwrightLauncher({ product: 'webkit' }),
+}
+
+// Prepend BROWSERS=x,y to `npm run test` to run a subset of browsers
+// e.g. `BROWSERS=chromium,firefox npm run test`
+const noBrowser = (b) => {
+  throw new Error(`No browser configured named '${b}'; using defaults`)
+}
+let commandLineBrowsers
+try {
+  commandLineBrowsers = process.env.BROWSERS?.split(',').map(
+    (browser) => browsers[browser] ?? noBrowser(browser)
+  )
+} catch (e) {
+  console.warn(e)
+}
 
 export default {
   nodeResolve: true,
   concurrency: 10,
-  plugins: [esbuildPlugin({ ts: true, target: 'auto' })],
-  browsers: [
-    ...(!env.PLAYWRIGHT_SKIP_CHROMIUM ? [playwrightLauncher({ product: 'chromium' })] : []),
-    ...(!env.PLAYWRIGHT_SKIP_FIREFOX ? [playwrightLauncher({ product: 'firefox' })] : []),
-    ...(!env.PLAYWRIGHT_SKIP_WEBKIT ? [playwrightLauncher({ product: 'webkit' })] : []),
+  browsers: commandLineBrowsers ?? Object.values(browsers),
+  filterBrowserLogs: ({ type }) => type !== 'warn',
+  reporters: [
+    defaultReporter({
+      reportTestResults: true,
+      reportTestProgress: true
+    }),
+    junitReporter({
+      outputPath: './junit-test-results.xml',
+      reportLogs: true,
+    }),
   ],
+  files: ['dist/**/*.test.js'],
   testRunnerHtml: (testFramework) => `
     <html lang="en-US">
       <head></head>
@@ -22,6 +50,5 @@ export default {
         <script type="module" src="${testFramework}"></script>
       </body>
     </html>
-  `,
-  files: ['src/**/*.test.ts'],
-};
+  `
+}
