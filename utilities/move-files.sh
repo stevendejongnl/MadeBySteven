c4585768199c1033f26c96e3ba5c12744f322c8c
Steven de jong 🍟 <sdejong@cloudsuite.com>
2024-06-04 22:34:08 +0200

    copy production and install next

--- utilities/move-files.sh
+++ utilities/move-files.sh
@@ -3,15 +3,19 @@
 if [ "$1" == "refs/heads/next" ]; then
   echo "Moving files to dist/next"
 
+  git clone https://github.com/stevendejongnl/MadeBySteven.git dist
+  git switch gh-pages
+
   mkdir -p dist/next
   mkdir -p dist/next/components
   mkdir -p dist/next/elements
   mkdir -p dist/next/helpers
 
-  cp CNAME dist/
   cp index.html dist/next
   cp -r pages/* dist/next
   cp -r dist/* dist/next
+
+  rm -rf dist/next/CNAME
 
   sed -i 's/\.\/dist\///g' dist/next/index.html
   sed -i 's/..\/dist\///g' dist/next/*.html
