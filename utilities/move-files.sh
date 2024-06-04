9da6353ca22b03706530e76d50bd36139cf5fe2b
Steven de jong 🍟 <sdejong@cloudsuite.com>
2024-06-04 20:34:00 +0200

    Add js files to next

--- utilities/move-files.sh
+++ utilities/move-files.sh
@@ -4,9 +4,20 @@ if [ "$1" == "refs/heads/next" ]; then
   echo "Moving files to dist/next"
 
   mkdir -p dist/next
+  mkdir -p dist/next/components
+  mkdir -p dist/next/elements
+  mkdir -p dist/next/helpers
+
   cp CNAME dist/
   cp index.html dist/next
   cp -r pages/* dist/next
+  cp -r components/* dist/next/components
+  cp -r elements/* dist/next/elements
+  cp -r helpers/* dist/next/helpers
+  cp *.css dist/next
+  cp *.js dist/next
+  cp *.d.ts dist/next
+
   sed -i 's/\.\/dist\///g' dist/next/index.html
   sed -i 's/..\/dist\///g' dist/next/*.html
   exit 0
