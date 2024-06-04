4017e6eb9265ef9c3a42fe76e28e64de44b94e88
stevendejongnl <stevendejongnl@users.noreply.github.com>
2024-06-04 18:05:48 +0000

    deploy: 45388496eccda35eff68da433f7fde837bfbb760

---
+++ next/components/made-a-header/made-a-header.d.ts
@@ -0,0 +1,5 @@
+import { LitElement } from 'lit';
+export declare class MadeAHeader extends LitElement {
+    static styles: import("lit").CSSResult;
+    render(): import("lit").TemplateResult<1>;
+}
---
+++ next/components/made-by-steven/made-by-steven.test.d.ts
@@ -0,0 +1,1 @@
+import './made-by-steven.js';
