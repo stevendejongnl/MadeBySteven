bbf4e754b36b39a4d577e033d12fd571308ea162
Steven de jong 🍟 <sdejong@cloudsuite.com>
2024-06-04 20:20:48 +0200

    change move script

--- utilities/move-files.sh
+++ utilities/move-files.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 
+NEXT_DIR=''
+
+if [ "$1" == "next" ]; then
+  echo "Moving files to dist/next"
+
+  mkdir -p dist/next
+  NEXT_DIR='next/'
+
+  exit 0
+fi
+
 cp CNAME dist/
-cp index.html dist/
-cp -r pages/* dist/
-sed -i 's/\.\/dist\///g' dist/index.html
-sed -i 's/..\/dist\///g' dist/*.html
+cp index.html dist/$NEXT_DIR
+cp -r pages/* dist/$NEXT_DIR
+sed -i 's/\.\/dist\///g' "dist/${NEXT_DIR}index.html"
+sed -i 's/..\/dist\///g' "dist/${NEXT_DIR}*.html"
