8031e2a35f5cf4ec33dfda7beb6163098bf37ce9
stevendejongnl <stevendejongnl@users.noreply.github.com>
2024-06-18 11:11:24 +0000

    deploy: a0340dfbbed99d4c5fb8a6368c0a775b0669386f

--- next/elements/made-my-name/made-my-name.d.ts
+++
@@ -1,5 +0,0 @@
-import { LitElement } from 'lit';
-export declare class MadeMyName extends LitElement {
-    static styles: import("lit").CSSResult;
-    render(): import("lit").TemplateResult<1>;
-}
