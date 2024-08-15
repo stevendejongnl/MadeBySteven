a67e461f680a77e36402366f3f7dc08d953dfaed
Steven de jong 🍟 <sdejong@cloudsuite.com>
2024-08-15 20:11:08 +0200

    New setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,117 +1,41 @@
-import chalk from 'chalk'
-import os from 'os'
-import { getLogFilter } from 'rollup/getLogFilter'
 import json from '@rollup/plugin-json'
+import typescript from '@rollup/plugin-typescript'
+import typescriptCompiler from 'ts-patch/compiler/typescript.js'
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
 import swc from '@rollup/plugin-swc'
-import { visualizer } from 'rollup-plugin-visualizer'
-import analyze from 'rollup-plugin-analyzer'
-
-const toBytes = (size) => {
-  const match = size.match(/(\d+(\.\d+)?)(\w+)/)
-  if (!match) {
-    return 0
-  }
-
-  const [, num, , unit] = match
-  const units = {
-    B: 1,
-    KB: 1e3,
-    MB: 1e6,
-    GB: 1e9,
-    TB: 1e12,
-  }
-
-  return parseFloat(num) * units[unit]
-}
-
-const limitBytes = toBytes('0.5MB')
-
-const onAnalysis = ({ bundleSize }) => {
-  if (bundleSize < limitBytes) {
-    return
-  }
-
-  console.warn(chalk.yellow(`Bundle size exceeds ${limitBytes} bytes: ${bundleSize} bytes`))
-
-  return
-}
-
-const analyzeFile = (input) => ({
-  input: input,
-  output: {
-    file: `${os.tmpdir()}/madebysteven/${input}`,
-  },
-  logLevel: 'silent',
-  plugins: [
-    analyze({
-      summaryOnly: true,
-      onAnalysis,
-    })
-  ]
-})
 
 const swcConfig = {
   minify: true,
   jsc: {
-    target: 'es2022'
+    target: "es2022"
   }
 }
 
-let thisIsUndefined = 0
-const bundleConfig = (input, namespace, outputName, minified = true) => ({
+const bundleIt = (input, name, outputFileName) => ({
   input: input,
   output: [
     {
-      file: `dist/${outputName}${minified ? '.min' : ''}.js`,
-      sourcemap: 'inline',
+      file: outputFileName,
       format: 'umd',
-      name: namespace,
+      sourcemap: true,
+      name: name,
     }
   ],
-  onLog(level, log, handler) {
-    const circularDependency = getLogFilter(['code:CIRCULAR_DEPENDENCY'])(log)
-    const theSentryCircularDependencyShit = log.message.includes('node_modules/@sentry/core')
-    // https://github.com/getsentry/sentry-javascript/issues/11553
-
-    if (circularDependency && theSentryCircularDependencyShit) {
-      return
-    }
-
-    if (getLogFilter(['code:THIS_IS_UNDEFINED'])(log)) {
-      thisIsUndefined++
-    }
-    if (thisIsUndefined > 0 && thisIsUndefined < 2) {
-      console.warn(chalk.yellow(log.message, '\n', log.url))
-      return
-    }
-    if (thisIsUndefined > 1) {
-      return
-    }
-
-    handler(level, log)
-  },
   plugins: [
     json(),
+    typescript({
+      typescript: typescriptCompiler,
+      sourceMap: true,
+    }),
     resolve(),
     commonjs(),
-    typescript({
-      tsconfig: 'tsconfig.json',
-    }),
-    visualizer({
-      filename: `dist/${outputName}${minified ? '-min' : ''}-stats.html`,
-      sourcemap: true
-    }),
-    minified ? swc({
+    swc({
       swc: swcConfig
-    }) : null,
-  ]
+    })
+  ],
 })
 
 export default [
-  bundleConfig('src/main.ts', 'MadeBySteven', 'bundle'),
-
-  analyzeFile('dist/bundle.min.js'),
+  bundleIt('src/main.ts', 'MadeBySteven', 'dist/main.js'),
 ]
--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 display_usage() {
   echo "Usage: bin/builder.sh [--watch=[typescript, rollup, styling]] [--only-types] [--cleanup] [--no-rollup] [--no-styling]"
-  echo "       npm run builder -- [--watch=[typescript, rollup, styling]] [--only-types] [--cleanup] [--no-rollup] [--no-styling]"
+  echo "       npm run build -- [--watch=[typescript, rollup, styling]] [--only-types] [--cleanup] [--no-rollup] [--no-styling]"
 }
 
 ARGUMENTS="$@"
@@ -12,23 +12,13 @@ if echo "$ARGUMENTS" | grep -q -- "--help"; then
   exit
 fi
 
-MONITORING="sh ./src/monitoring/prepare.sh"
-ANALYZE="npm run analyze"
 TYPESCRIPT_COMPILE="tspc -b"
 STYLING="sass --no-source-map src/main.scss dist/main.css"
-STYLING_MINIFIED="sass --embed-sources ./src/main.scss ./dist/bundle.min.css --style=compressed"
+STYLING_MINIFIED="sass --embed-sources ./src/main.scss ./dist/main.css --style=compressed"
 ROLLUP="rollup --config ./rollup.config.js"
 
 if echo "$ARGUMENTS" | grep -q -- "--cleanup"; then
   npm run clean
-fi
-
-# $MONITORING 
-
-if echo "$ARGUMENTS" | grep -q -- "--watch=typescript"; then
-  $TYPESCRIPT_COMPILE --watch
-  TYPESCRIPT_COMPILE_EXIT_CODE=$?
-  exit $(($TYPESCRIPT_COMPILE_EXIT_CODE + $TYPESCRIPT_DTS_FILES_EXIT_CODE))
 fi
 
 if echo "$ARGUMENTS" | grep -q -- "--watch=rollup"; then
@@ -75,8 +65,4 @@ echo "Run $STYLING"
 $STYLING
 STYLING_EXIT_CODE=$?
 
-# echo "Run $ANALYZE"
-# $ANALYZE
-ANALYZE_EXIT_CODE=$?
-
-exit $(($TYPESCRIPT_COMPILE_EXIT_CODE + $TYPESCRIPT_DTS_FILES_EXIT_CODE + $ROLLUP_EXIT_CODE + $STYLING_MINIFIED_EXIT_CODE + $STYLING_EXIT_CODE + $ANALYZE_EXIT_CODE))
+exit $(($TYPESCRIPT_COMPILE_EXIT_CODE + $TYPESCRIPT_DTS_FILES_EXIT_CODE + $ROLLUP_EXIT_CODE + $STYLING_MINIFIED_EXIT_CODE + $STYLING_EXIT_CODE))
