978eb3c09331753a919bbc0ba2ddfbe7c98f79b2
stevendejongnl <stevendejongnl@users.noreply.github.com>
2024-08-15 18:51:37 +0000

    deploy: 08ad8703c568689d97018ce3d1e4a0f10225513f

--- src/helpers/functions.ts
+++
@@ -1,5 +0,0 @@
-import {css} from 'lit'
-
-export const rem = (px: number, base = 16) => {
-  return css`${(1 / base * px)}rem`
-}
--- src/main.ts
+++
@@ -1,2 +0,0 @@
-export * from './components/main.js'
-export * from './elements/main.js'
