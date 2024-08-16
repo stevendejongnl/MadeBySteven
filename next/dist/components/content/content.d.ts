ccc86a1ff9a1f0a6358c13179e062d56c966ad89
stevendejongnl <stevendejongnl@users.noreply.github.com>
2024-08-16 21:02:13 +0000

    deploy: b8db2d05bf9e32d375a4517d11c3fbb73ac2c178

--- next/dist/components/content/content.d.ts
+++ next/dist/components/content/content.d.ts
@@ -1,1 +1,5 @@
-export {};
+import { LitElement } from 'lit';
+export declare class MBSContent extends LitElement {
+    static styles: import("lit").CSSResult;
+    render(): import("lit-html").TemplateResult<1>;
+}
---
+++ next/dist/components/github/repositories.d.ts
@@ -0,0 +1,6 @@
+import { LitElement } from 'lit';
+export declare class MBSGithubRepositories extends LitElement {
+    private repositories;
+    connectedCallback(): void;
+    render(): import("lit-html").TemplateResult<1>;
+}
