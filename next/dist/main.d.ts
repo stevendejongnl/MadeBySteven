f2c00a9420f8146276a38c11373a7101f77c518d
stevendejongnl <stevendejongnl@users.noreply.github.com>
2024-08-16 18:38:15 +0000

    deploy: 833674877241c660cfe5b089bc27969d21b8acb6

--- next/dist/main.d.ts
+++ next/dist/main.d.ts
@@ -1,2 +1,5 @@
-export * from 'madebysteven/madebysteven.js';
+export * from 'components/madebysteven/madebysteven.js';
+export * from 'components/header/header.js';
+export * from 'components/content/content.js';
+export * from 'components/footer/footer.js';
 export declare const VERSION: string;
